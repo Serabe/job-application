00e5fb7c44e4a23b753229035454203e4f2cc162
Sergio Arbeo <serabe@gmail.com>
2010-07-03 03:31:10 +0200

    First version of github-challenge

---
+++ github-challenge/script.rb
@@ -0,0 +1,39 @@
+require 'erb'
+require 'uri'
+require 'net/http'
+require 'yaml'
+
+TEMPLATE_FILE = File.join(File.dirname(__FILE__), 'template.html.erb')
+OUTPUT_FILE = File.join(File.dirname(__FILE__), 'rails_committers.html')
+USER_ID = :rails
+REPO = :rails
+
+def github(*path)
+  "http://github.com/api/v2/yaml/#{path.join('/')}"
+end
+
+rhtml = ERB.new(File.read(TEMPLATE_FILE))
+
+commits = YAML.load(Net::HTTP.get_response(URI.parse(github(:commits, :list,  USER_ID, REPO, :master))).body)["commits"]
+
+class LastCommits
+
+  def initialize(commits)
+    @email_to_name = {}
+    @email_to_commits = {}
+    commits.each do |ci|
+      email = ci["author"]["email"].to_sym
+      @email_to_name[email] = ci["author"]["name"] unless @email_to_name[email]
+      (@email_to_commits[email] ||= []) << ci["message"]
+    end
+  end
+
+  def get_binding
+    binding
+  end
+end
+
+File.open(OUTPUT_FILE, 'w') do |f|
+  f.puts rhtml.result(LastCommits.new(commits).get_binding)
+end
+
